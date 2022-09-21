b23de80d8e53014d044a1288127ed193d072c8d0
jonalobo <jonathan_1920@hotmail.com>
2022-09-21 15:14:54 -0600

    Correccion y eliminación del componente restante Item

--- src/components/Itemlist.js
+++ src/components/Itemlist.js
@@ -1,9 +1,29 @@
-import Item from "../components/Item";
+import { Link } from "react-router-dom";
+import "../css/Item.css";
 
 const Itemlist = ({ datos }) => {
   return (
     <>
-      <Item products={datos} />
+      {datos.map((e) => (
+        <div
+          className="card-compact w-96 bg-base-100 shadow-xl separacion"
+          key={e.id}
+        >
+          <figure className="px-10 pt-10">
+            <img src={e.imageId} alt="Shoes" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">{e.title}</h2>
+            <p>{e.description}</p>
+            <h1 className="precio">{e.price}$</h1>
+            <div className="card-actions">
+              <button className="btn btn-primary">
+                <Link to={"/item/" + e.id}>Detalles</Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
 };
